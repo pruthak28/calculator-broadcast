85e8fdf083145ad647b1acab191fbea1cfa36106
Prutha Kulkarni <pruthakulkarni@Pruthas-MacBook-Air.local>
2020-07-10 20:21:23 -0400

    hosting

---
+++ precache-manifest.38dfb44fafe110c7fb48dfc80a98b313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea729f850a9295d9252c0e73b0e6cee",
+    "url": "/calculator-broadcast/index.html"
+  },
+  {
+    "revision": "9bcc7d624c501554ce2b",
+    "url": "/calculator-broadcast/static/js/2.8cdfb634.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/calculator-broadcast/static/js/2.8cdfb634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b822e7e97eec216a54b",
+    "url": "/calculator-broadcast/static/js/main.6ae907ed.chunk.js"
+  },
+  {
+    "revision": "deaa0ae254d807a5acae",
+    "url": "/calculator-broadcast/static/js/runtime-main.f95ae368.js"
+  }
+]);
